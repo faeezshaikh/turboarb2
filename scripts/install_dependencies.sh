8864abf08a0a6c9e5e7c8dda730c22357cbcf1c5
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2017-04-07 12:00:03 -0500

    Update npm location

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/Ionic2App
 #rm -rf node_modules
-#/usr/local/bin/npm install @ionic/app-scripts@latest --save-dev
-#/usr/local/bin/npm install
-ls -al
+/usr/local/bin/npm install @ionic/app-scripts@latest --save-dev
+/usr/local/bin/npm install
+#ls -al
