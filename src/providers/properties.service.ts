9cf144e9632ed77592ba2edab51b63ab1fabce61
Faeez Shaikh <faeez.shaikh@gmail.com>
2017-05-11 20:33:24 -0500

    cognito updated

--- src/providers/properties.service.ts
+++ src/providers/properties.service.ts
@@ -4,9 +4,14 @@
 
 export let _REGION = "us-west-2";
 
-export let _IDENTITY_POOL_ID = "us-west-2:956815528383";
-export let _USER_POOL_ID = "us-west-2_N6hhp8npU";
-export let _CLIENT_ID = "1jo0u1f1dsec0d77ihflui1l9t";
+// export let _IDENTITY_POOL_ID = "us-west-2:956815528383";
+// export let _USER_POOL_ID = "us-west-2_N6hhp8npU";
+// export let _CLIENT_ID = "1jo0u1f1dsec0d77ihflui1l9t";
+// export let _MOBILE_ANALYTICS_APP_ID = "8ed115bcfac94dfa8e364aa5ef201849";
+
+export let _IDENTITY_POOL_ID = "us-west-2:490831136015";
+export let _USER_POOL_ID = "us-west-2_LEhYDbMOT";
+export let _CLIENT_ID = "3vkvg7u3fm51vb9ic9d3kb7gbb";
 export let _MOBILE_ANALYTICS_APP_ID = "8ed115bcfac94dfa8e364aa5ef201849";
 
 export let _POOL_DATA = {
